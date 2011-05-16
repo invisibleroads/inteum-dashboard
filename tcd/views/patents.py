65f2b3f873789ff5c43895d86ec221dab458b486
Roy Hyunjin Han <support@invisibleroads.com>
2011-05-15 20:39:19 -0400

    Fixed small bug

--- tcd/views/patents.py
+++ tcd/views/patents.py
@@ -2,7 +2,7 @@
 from pyramid.view import view_config
 from sqlalchemy.orm import joinedload
 
-from tcd.models import db, Upload, Patent, PatentType, PatentStatus
+from tcd.models import db, Upload, Patent, PatentStatus, PatentType
 
 
 def includeme(config):
@@ -14,5 +14,16 @@ def includeme(config):
 def index(request):
     'Display patent activity'
     when_uploaded = db.query(Upload).order_by(Upload.when.desc()).first()
-    patents = db.query(Patent).options(joinedload(Patent.technology, Patent.firm, Patent.status, Patent.type, Patent.inventors, Patent.country)).order_by(Patent.status.name, Patent.type.name, Patent.date_filed).all()
+    patents = db.query(Patent).join(Patent.status, Patent.type).options(joinedload(
+        Patent.technology, 
+        Patent.firm, 
+        Patent.status, 
+        Patent.type, 
+        Patent.inventors, 
+        Patent.country,
+    )).order_by(
+        PatentStatus.name,
+        PatentType.name,
+        Patent.date_filed,
+    ).all()
     return dict(when_uploaded=when_uploaded, patents=patents)
